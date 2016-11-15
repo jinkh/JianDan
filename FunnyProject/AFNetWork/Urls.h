e1bac053642fa98a74954aab5f98c4165eaac9ff
jinkh <542745553@qq.com>
2016-11-15 11:55:40 +0800

    fix net

--- FunnyProject/AFNetWork/Urls.h
+++ FunnyProject/AFNetWork/Urls.h
@@ -10,19 +10,19 @@
 #define Urls_h
 
 //新鲜事列表
-static NSString* const FreshNewUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=get_recent_posts&include=url,date,tags,author,title,comment_count,custom_fields&custom_fields=thumb_c,views&dev=1";
+static NSString* const FreshNewUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=get_recent_posts&include=url,date,tags,author,title,comment_count,custom_fields&custom_fields=thumb_c,views&dev=1";
 
 //无聊图列表
-static NSString* const BoredPicturesUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments&page=";
+static NSString* const BoredPicturesUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_pic_comments&page=";
 
 //妹子图
-static NSString* const SisterPicturesUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments&page=";
+static NSString* const SisterPicturesUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_ooxx_comments&page=";
 
 //段子
-static NSString* const JokeUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_duan_comments&page=";
+static NSString* const JokeUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_duan_comments&page=";
 
 //小视频
-static NSString* const LittleMovieUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=jandan.get_video_comments&page=";
+static NSString* const LittleMovieUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=jandan.get_video_comments&page=";
 
 //评论数量
 static NSString* const CommentCountUrl = @"http://jandan.duoshuo.com/api/threads/counts.json?threads=";
@@ -31,13 +31,13 @@ static NSString* const CommentCountUrl = @"http://jandan.duoshuo.com/api/threads
 static NSString* const FreshNewDetailUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=get_post&include=content&id=";
 
 //新鲜事评论
-static NSString* const FreshNewCommentlUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=get_post&include=comments&id=";
+static NSString* const FreshNewCommentlUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=get_post&include=comments&id=";
 
 //发表评论接口
-static NSString* const PushCommentlUrl = @"http://jandan.net/?oxwlxojflwblxbsapi=respond.submit_comment";
+static NSString* const PushCommentlUrl = @"http://i.jandan.net/?oxwlxojflwblxbsapi=respond.submit_comment";
 
 //投票
-static NSString* const CommentVoteUrl=@"http://jandan.net/index.php?acv_ajax=true&option=%@&ID=%@";
+static NSString* const CommentVoteUrl=@"http://i.jandan.net/index.php?acv_ajax=true&option=%@&ID=%@";
 
 //多说评论列表 无聊图
 static NSString* const DuoShuoCommentListlUrl=@"http://jandan.duoshuo.com/api/threads/listPosts.json?thread_key=";
